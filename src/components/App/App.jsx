18de3ef249f28356dfe5bacde5600742e835bea9
Ryan Weispfenning <weisp014@gmail.com>
2023-03-30 14:51:32 -0500

    added routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,52 +1,51 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
-import CustomerForm from '../CustomerForm/CustomerForm';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import PizzaList from '../PizzaList/PizzaList';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import CustomerForm from "../CustomerForm/CustomerForm";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import PizzaList from "../PizzaList/PizzaList";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     fetchPizza();
-  })
+  });
 
-
-
-const fetchPizza = () => {
-  console.log('inside fetchPizza()')
-  axios.get('/api/pizza')
-  .then(response => {
-    dispatch({ type: 'GET_PIZZA', payload: response.data})
-  })
-  .catch(error => {
-    console.log('error')
-  })
-}
-
-
-
+  const fetchPizza = () => {
+    console.log("inside fetchPizza()");
+    axios
+      .get("/api/pizza")
+      .then((response) => {
+        dispatch({ type: "GET_PIZZA", payload: response.data });
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Prime Pizza</h1>
       </header>
-      {/* Get pizza list and display */}
-      <PizzaList />
+      <Router>
+        {/* Get pizza list and display */}
+        <Route exact path="/">
+        <PizzaList />
+        </Route>
 
-      {/* Customer Form */}
-      <CustomerForm />
-      {/* Checkout */}
+        {/* Customer Form */}
+        <Route path="custInfo">
+        <CustomerForm />
+        </Route>
+        {/* Checkout */}
 
-      {/* Admin */}
-  
-      {/* <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p> */}
-  
+        {/* Admin */}
+
+      </Router>
     </div>
   );
 }
