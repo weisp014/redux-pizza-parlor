217717b27df98c57243917fdad84d05d08532e0e
Ryan Weispfenning <weisp014@gmail.com>
2023-03-30 13:46:44 -0500

    completed customer form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import CustomerForm from '../CustomerForm/CustomerForm';
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
       {/* Get pizza list and display */}
 
       {/* Customer Form */}
-
+      <CustomerForm />
       {/* Checkout */}
 
       {/* Admin */}
--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import logger from "redux-logger";
 
+const customerInfo = (state = {}, action) => {
+    if (action.type === 'NEW_CUSTOMER') {
+        console.log('customer info:', action.payload);
+        return action.payload;
+    }
+    return state;
+}
+
 const storeInstance = createStore(
   combineReducers({
     //list of reducers here
-    
+    customerInfo,
+
   }),
   applyMiddleware(logger)
 );
