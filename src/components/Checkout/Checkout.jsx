64b288897e7081eae7b0ed3950353e2227531d00
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-03-30 16:54:37 -0500

    added checkout and admin sections

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -18,7 +18,7 @@ function Checkout() {
         return sum
         
     }
-    
+    let checkoutTotal = totalPrice(checkoutCart)
     
 
     const handleCheckout = () => {
@@ -33,8 +33,8 @@ function Checkout() {
             street_address: checkoutCustomerInfo.address,
             city: checkoutCustomerInfo.city,
             zip: checkoutCustomerInfo.zip,
-            total: "27.98",
-            type: "Pickup",
+            total: checkoutTotal,
+            type: checkoutCustomerInfo.deliveryOption,
             // these are really irrelevant pre-stretch goals.
             pizzas: [{
                 id: '1',
@@ -91,7 +91,7 @@ function Checkout() {
                 </tbody>
             </table>
             <div>
-                Total: ${totalPrice(checkoutCart)}
+                Total: ${checkoutTotal}
             </div>
             <button onClick={handleCheckout}>Checkout</button>
         </>
