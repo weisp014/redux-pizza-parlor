1a1f9df37d1d4d401e6192848ca476fd32306429
Ryan Weispfenning <weisp014@gmail.com>
2023-03-31 11:00:39 -0500

    updated styling for header

--- src/components/HeaderWithCart/HeaderWithCart.jsx
+++ src/components/HeaderWithCart/HeaderWithCart.jsx
@@ -1,26 +1,25 @@
-import { useSelector } from "react-redux"
-
+import { useSelector } from "react-redux";
 
 function HeaderWithCart() {
-    const cart = useSelector(store => store.cart)
+  const cart = useSelector((store) => store.cart);
 
-    const calculateTotal = () => {
-        let total = 0
-        cart.map((item) => {
-            total += Number(item.price)
-            console.log('looking at item price', total)
+  const calculateTotal = () => {
+    let total = 0;
+    cart.map((item) => {
+      total += Number(item.price);
+      console.log("looking at item price", total);
+    });
+    return total;
+  };
 
-        }
-        )
-        return total
-    }
-
-    return (
-        <header className='App-header'>
-            <h1 className='App-title'>Prime Pizza</h1>
-            <h3>Total: {calculateTotal()}</h3>
-        </header>
-    )
+  return (
+    <header className="App-header">
+      <div className="title-combo">
+        <h1 className="App-title">Prime Pizza</h1>
+        <h3 className="total">Total: ${calculateTotal()}</h3>
+      </div>
+    </header>
+  );
 }
 
-export default HeaderWithCart
+export default HeaderWithCart;
